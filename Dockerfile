5a00be5ae21020bbf84a344badd18bcbaef19592
kingapex@javashop.cn <kingapex@javashop.cn>
2022-04-14 12:02:59 +0800

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
-# 注意：请在ui目录下执行build命令
-# 例如：docker build -t buyer-pc:v1 -f ./buyer/pc/Dockerfile .
-
 FROM node:8.10.0-slim
-MAINTAINER Javashop
+MAINTAINER Shopflix
 
-RUN mkdir -p /ui/buyer/pc
-RUN mkdir -p /ui/ui-domain
-RUN mkdir -p /ui/ui-utils
-COPY ./buyer /ui/buyer
-COPY ./ui-domain /ui/ui-domain
-COPY ./ui-utils /ui/ui-utils
+RUN mkdir -p /app
+COPY ./ /app
 
-WORKDIR /ui/buyer/pc
+WORKDIR /app
 
 RUN npm install --registry=https://registry.npm.taobao.org
 
--- ui-domain/api.js
+++ ui-domain/api.js
@@ -1,28 +1,28 @@
 /**
  * Created by Andste on 2018/7/2.
- * base    : 基础业务API
- * buyer   : 买家API
- * seller  : 商家中心API
- * admin   ：后台管理API
+ * base    : Based on the businessAPI
+ * buyer   : buyersAPI
+ * seller  : Business centerAPI
+ * admin   ：Background managementAPI
  */
 
 let env = process.server
   ? process.env
   : (global.window && window.__NUXT__.state ? window.__NUXT__.state.env : {})
 
-// 开发环境
+// The development environment
 export const dev = {
-  base: 'https://api-b2c-v710.javamall.com.cn/base',
-  buyer: 'https://api-b2c-v710.javamall.com.cn/buyer',
+  base: 'http://52.53.155.58',
+  buyer: 'http://52.53.155.58',
   seller: 'https://api-b2c-v710.javamall.com.cn/seller',
   live: 'https://api-b2c-v710.javamall.com.cn/live',
   im: 'https://api-b2c-v710.javamall.com.cn/im'
 }
 
-// 生产环境
+// The production environment
 export const pro = {
-  base: env.API_BASE || 'https://api-b2c-v710.javamall.com.cn/base',
-  buyer: env.API_BUYER || 'https://api-b2c-v710.javamall.com.cn/buyer',
+  base: env.API_BASE || 'http://api:8080',
+  buyer: env.API_BUYER || 'http://api:8080',
   seller: env.API_SELLER || 'https://api-b2c-v710.javamall.com.cn/seller',
   live: env.API_LIVE || 'https://api-b2c-v710.javamall.com.cn/live',
   im: env.API_IM || 'https://api-b2c-v710.javamall.com.cn/im'
